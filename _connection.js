47e02e7e23a6b6b3fb3f2d5b517d734fe3484374
Vitaly Gins <vitaly.gins@gmail.com>
2021-03-21 09:43:27 -0600

    created error partial and route

--- _connection.js
+++ _connection.js
@@ -1,5 +1,5 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
-const dotenv = require('dotenv').config();
 
 mongoose
   .connect(process.env.MONGODB_URL, { useUnifiedTopology: true, useNewUrlParser: true })
--- server.js
+++ server.js
@@ -8,9 +8,8 @@ const path = require('path');
 
 const routes = require('./routes');
 
-// Read the host address and the port from the environment
-const hostname = process.env.HOST;
-const port = process.env.PORT || 3000;
+// Read the port from the environment
+const PORT = process.env.PORT || 3000;
 
 //Every time we see a app.use we apply a Middleware in express
 //Returns the static asset as long as it is found in the specified folder.
@@ -27,7 +26,24 @@ app.use('/', (req, res, next) => {
 app.use('/', routes());
 
 //404 Not Found page.
-app.use((req, res) => res.status(404).redirect('404.html'));
+app.use((req, res, next) => {
+  const err = new Error('Requested page could not be found');
+  err.status = 404;
+  next(err);
+});
+
+// Catch all the errors that might have occurred
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  // Save the error, but not display it to the user.
+  console.error(err);
+  // Default to a DB error
+  const status = err.status || 500;
+  res.locals.status = status;
+  res.status(status);
+  // Render the error page using the default layout
+  res.render('pages/error', { pageTitle: `Error ${status}`, template: 'error' });
+});
 
 // Start a TCP server listening for connections on the given port and host
-app.listen(port, hostname, () => console.log(`Server running at http://${hostname}:${port}/`));
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}/`));
