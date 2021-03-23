4a0bb1c014f2e516845dbf9de784907352ec7e92
kylewwelsh <kyle@kdmdental.com>
2021-03-22 20:53:14 -0600

    drink recipie js added for 3 rum and 3 tequila

---
+++ data/drinkRecipies.js
@@ -0,0 +1,73 @@
+// Rum Drinks
+{
+  name: 'Rum and Coke',
+  baseAlcohol: [{ name: 'Rum', quantity: 2, unit: 'oz' }],
+  Ingredients: [{ name: 'Coke', quantity: 5, unit: 'oz' }],
+  Garnish: [{ name: 'Lime Wedge', quantity: 1}],
+  glassType: 'Short Glass',
+  Recipe: 'Fill glass with ice, mix in Rum first then Coke. Garnish with the lime wedge and enjoy!',
+  imagePath: '/images/lg/image.jpg',
+  width: 1080,
+  height: 400,
+},
+
+{
+  name: 'Rum Runner',
+  baseAlcohol: [{ name: 'Light Rum', quantity: 1, unit: 'oz', name: 'Dark Rum', quantity: 1, unit: 'oz', name: 'Blackberry Liqeur', quantity 0.5, unit: 'oz', name: 'Banana Liqeur', quantity: 1, unit: 'oz' }],
+  Ingredients: [{ name: 'Orange Juice', quantity: 1, unit: 'oz', name: 'Pineapple Juice', quantity: 1, unit: 'oz' }],
+  Garnish: [{ name: 'Fresh Fruit' }],
+  glassType: 'Tall Glass',
+  Recipe: 'Poor each alcohol into a cocktail mixer filled with ice and shake well. Strain mix into tall glass over fresh ice and mix in desired amount of fruit.',
+  imagePath: '/images/lg/image.jpg',
+  width: 1080,
+  height: 400,
+},
+
+{
+  name: 'Dark and Stormy',
+  baseAlcohol: [{ name: 'Dark Rum', quantity: 1, unit: 'oz', name: 'Ginger Beer', quantity: 100, unit: 'mL' }],
+  Ingredients: [{ name: 'Lime Juice', quantity: 25, unit: 'mL' }],
+  Garnish: [{ name: 'Angostura Bitters', quantity: 2, unit: 'dashes', name: 'Lime Wedge', quantity: 1 }],
+  glassType: 'Tall Glass',
+  Recipe: 'Poor alcohol followed by Lime juice into glass, mix slowly with a spoon then garnish with a lime wedge.',
+  imagePath: '/images/lg/image.jpg',
+  width: 1080,
+  height: 400,
+},
+
+// Tequila Drinks
+{
+  name: 'Margarita',
+  baseAlcohol: [{ name: 'Tequila', quantity: 1.5, unit: 'oz', name: 'Tripple Sec', quantity: 1, unit: 'oz' }],
+  Ingredients: [{ name: 'Lime Juice', quantity: 0.75, unit: 'oz' }],
+  Garnish: [{ name: 'Sugar or Salt' }],
+  glassType: 'Margarita Glass',
+  Recipe: 'Wet the rim of your margarita glass then stick the sugar to the rim. Pour the Lime Juice, Tequila and Tripple Sec into a cocktail mixer over ice and shake well. Strain the contents in the cocktail mixer into your margarita glass, fresh ice is optional in the glass. ',
+  imagePath: '/images/lg/image.jpg',
+  width: 1080,
+  height: 400,
+},
+
+{
+  name: 'Tequila Sunrise',
+  baseAlcohol: [{ name: 'Tequila', quantity: 2, unit: 'oz' }],
+  Ingredients: [{ name: 'Orange Juice', quantity: 4, unit: 'oz', name: 'Grenadine', quantity: 0.5, unit: 'oz' }],
+  Garnish: [{ name: 'Orange Slice', quantity: 1, name: 'Maraschino Cherry', quantity: 1 }]
+  glassType: 'Tall Glass',
+  Recipe: 'Pour the Tequila and Orange Juice into the glass over ice, stir well and then slowly poor Grenadine around the inside edge of the glass. Garnish with the Orange Slice and Cherry, do not mix or stir after pooring the Grenadine.',
+  imagePath: '/images/lg/image.jpg',
+  width: 1080,
+  height: 400,
+},
+
+{
+  name: 'The Paloma',
+  baseAlcohol: [{ name: 'Tequila', quantity: 2, unit: 'oz' }],
+  Ingredients: [{ name: 'Lime Juice', quantity: 0.25, unit: 'oz', name: 'Grapefruit Soda', quantity: 4, unit: 'oz' }],
+  Garnish: [{ name: 'Lime wedge', quantity: 1, name: 'salt' }],
+  glassType: 'Tall Glass',
+  Recipe: 'Wet and salt the rim of the glass then halfway with ice. Add the Tequila, Lime Juice then Grapfruit and enjoy!',
+  imagePath: '/images/lg/image.jpg',
+  width: 1080,
+  height: 400,
+},
