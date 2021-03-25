cdf8d0ab5f9d6e439fe92ce78538b854e2c740be
Vitaly Gins <vitaly.gins@gmail.com>
2021-03-25 14:10:53 -0600

    changed team partial

--- public/js/team.mjs
+++ public/js/team.mjs
@@ -1,33 +1,36 @@
 import getRequest from '/js/get-json.mjs';
-// fetch("/api/v0/members")
-
-//   .then(function (response) {
-//     if (!response.ok) {
-//       throw new Error("This is an error");
-//     }
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     console.log(data);
+// <div class="card">
+//   <img src="" alt="a person" class="profile-image" />
+//   <h2 class="name"> </h2>
+//   <p class="role"> </p>
+//   <p class="bio"> </p>
+//   <div class="social-links">
+//     <a href="#">
+//       <i class="fab fa-github"></i>
+//     </a>
+//   </div>
+// </div>;
 const getTeam = async () => {
-    const data = await getRequest('/api/v0/members');
-    const teamMember = data;
-    const card = document.querySelector(".card");
-    //create output string for html
-    let output='';
-    teamMember.forEach(function(item){
-      output += `
-        <img class ="profile-image" src="${item.profilePic}" alt="${item.name}">
-        <h2> ${item.name} </h2>
-        <p>${item.role} </p>
-        <p>${item.bio} </p>
-        <a href="#" ${item.github} ><i class="fab fa-github"></i></a> `;
-  
-      document.querySelector('.card').innerHTML= output;
+  const data = await getRequest('/api/v0/members');
+  const teamMember = data;
+  const card = document.querySelector('.row.team');
+  //create output string for html
+  let output = '';
+  teamMember.forEach(function (item) {
+    output += `
+      <div class="member">
+        <div class="card">
+          <img class ="profile-image" src="${item.profilePic}" alt="${item.name}">
+          <h2> ${item.name} </h2>
+          <p>${item.role} </p>
+          <p>${item.bio} </p>
+          <div class="social-links">
+            <a href="#" ${item.github} ><i class="fab fa-github"></i></a> 
+          </div>
+        </div>
+      </div>`;
+    card.innerHTML = output;
   });
-}
+};
 
 getTeam();
-// .catch(function (error) {
-//   console.log(error);
-// });
