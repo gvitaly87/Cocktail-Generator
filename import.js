4955e4de76fdb22a12fe3767c26eeb09b68db4e5
Vitaly Gins <vitaly.gins@gmail.com>
2021-03-26 15:51:02 -0600

    deleted some files from data folder

--- import.js
+++ import.js
@@ -4,9 +4,11 @@ const mongoose = require('mongoose');
 // Import seed data
 const drinkSeed = require(`./seeds/drinks`);
 const teamSeed = require(`./seeds/team`);
+const subsSeed = require(`./seeds/subscribers`);
 
 // Define model
 const Image = require(`./models/DrinkModel`);
+const Subscriber = require(`./models/SubscriberModel`);
 const TeamMember = require(`./models/TeamMemberModel`);
 
 /*******************************/
@@ -28,12 +30,16 @@ db.once('open', function () {
   console.log('Connected to DB...');
 });
 
-Image.insertMany(drinkSeed, function (error, definition) {
-  console.log('Data import completed.');
-  mongoose.connection.close();
-});
+// Image.insertMany(drinkSeed, function (error, definition) {
+//   console.log('Data import completed.');
+//   mongoose.connection.close();
+// });
 
 // TeamMember.insertMany(teamSeed, function (error, definition) {
 //   console.log('Data import completed.');
 //   mongoose.connection.close();
 // });
+Subscriber.insertMany(subsSeed, function (error, definition) {
+  console.log('Data import completed.');
+  mongoose.connection.close();
+});
