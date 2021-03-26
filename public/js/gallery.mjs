2d424b09a1786729da47d6c2d4e75f0877e69de5
Vitaly Gins <vitaly.gins@gmail.com>
2021-03-26 17:08:33 -0600

    added page pagination

--- public/js/gallery.mjs
+++ public/js/gallery.mjs
@@ -1,43 +1,56 @@
 import getRequest from '/js/get-json.mjs';
 
-const image = document.querySelector('image');
-const button = document.querySelector('button');
-
-const getDrinks = async () => {
-  const data = await getRequest('/api/v0/gallery');
+let pageCount;
+const getDrinks = async (page, limit) => {
+  const data = await getRequest(`/api/v0/gallery?page=${page}&limit=${limit}`);
   const drinkGenerator = data.drinkList;
-
-  let output = '';
+  pageCount = data.totalPages;
+  let output = ``;
 
   drinkGenerator.forEach(function (drink) {
-    // output += ` <h2 class="name"${drink.name}></h2>
-    // <div class="image"><img src="${drink.image}" alt="${drink.name}"></div>
-    // <div class="baseAlcohol">${drink.baseAlcohol}</div>
-    // <div class="glassType">${drink.glassType}</div>
-    // <div class="ingredients">${drink.ingredients}</div>
-    // <div class="recipe"><p>${drink.recipe}</p></div> `;
-
     output += `
     <div class="recipe-card">
-      <figure>
-        <img src="${drink.imgPath}" alt="${drink.name}">
-      </figure>
-      <div class="recipe-data">`;
+    <figure>
+    <img src="${drink.imgPath}" alt="${drink.name}">
+    </figure>
+    <div class="recipe-data">`;
 
     for (let i = 0; i < drink.baseAlcohol.length; i++) {
       output += `
         <p class="alcohol-type">${drink.baseAlcohol[i].name}</p>`;
     }
-    output +=`
+    output += `
       <h1 class="drink-name">Whiskey Sour</h1>
       <div class="star-rating">
-        <i class="fas fa-star"></i>
-        <p class="rating">4.5</p>
+      <i class="fas fa-star"></i>
+      <p class="rating">4.5</p>
       </div>
     </div>
-  </div>`
+    </div>`;
   });
   document.querySelector('.drink').innerHTML = output;
 };
 
-getDrinks();
+const deleteDrinks = () => {
+  document.querySelectorAll('.recipe-card').forEach((e) => e.parentNode.removeChild(e));
+};
+
+let page = 1;
+let entriesPerPage = 12;
+getDrinks(page, entriesPerPage);
+
+document.querySelector('.prev').addEventListener('click', () => {
+  if (page > 1) {
+    page -= 1;
+    deleteDrinks();
+    getDrinks(page, entriesPerPage);
+  }
+});
+
+document.querySelector('.next').addEventListener('click', () => {
+  if (page < pageCount) {
+    page += 1;
+    deleteDrinks();
+    getDrinks(page, entriesPerPage);
+  }
+});
