409aa5af436a368cdaffa52001b6a13a40b36c61
Vitaly Gins <vitaly.gins@gmail.com>
2021-03-26 17:27:01 -0600

    added a page counter

--- public/js/gallery.mjs
+++ public/js/gallery.mjs
@@ -1,12 +1,16 @@
 import getRequest from '/js/get-json.mjs';
 
 let pageCount;
+// Inserts drinks into the page(based on the page number and the amount of drinks/page)
 const getDrinks = async (page, limit) => {
   const data = await getRequest(`/api/v0/gallery?page=${page}&limit=${limit}`);
   const drinkGenerator = data.drinkList;
   pageCount = data.totalPages;
-  let output = ``;
 
+  // Set page counter inside the page
+  document.querySelector('.page').innerHTML = `Page ${page} of ${pageCount}`;
+
+  let output = ``;
   drinkGenerator.forEach(function (drink) {
     output += `
     <div class="recipe-card">
@@ -30,15 +34,17 @@ const getDrinks = async (page, limit) => {
   });
   document.querySelector('.drink').innerHTML = output;
 };
-
+//Deletes all the drink entries inside the gallery
 const deleteDrinks = () => {
   document.querySelectorAll('.recipe-card').forEach((e) => e.parentNode.removeChild(e));
 };
 
+//Default for the gallery to start on page 1 and have 12 items per page
 let page = 1;
 let entriesPerPage = 12;
 getDrinks(page, entriesPerPage);
 
+/***********Page Pagination Buttons*********/
 document.querySelector('.prev').addEventListener('click', () => {
   if (page > 1) {
     page -= 1;
