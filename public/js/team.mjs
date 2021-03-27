0be230649db9f34119d578a0504564b5e5b8f9ef
Vitaly Gins <vitaly.gins@gmail.com>
2021-03-27 17:56:53 -0600

    updated teampage front end js to use favdrink

--- public/js/team.mjs
+++ public/js/team.mjs
@@ -22,8 +22,8 @@ const getTeam = async () => {
         <div class="card">
           <img class ="profile-image" src="${item.profilePic}" alt="${item.name}">
           <h2> ${item.name} </h2>
-          <p>${item.role} </p>
-          <p>${item.bio} </p>
+          <p>Favorite Drink: ${item.favoriteDrink} </p>
+          <p>Bio: ${item.bio} </p>
           <div class="social-links">
             <a href="#" ${item.github} ><i class="fab fa-github"></i></a> 
           </div>
