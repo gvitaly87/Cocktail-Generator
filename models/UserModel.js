17f6a348fc3c86db9e9a5ef82943262f433d41b3
Vitaly Gins <vitaly.gins@gmail.com>
2021-03-27 12:34:34 -0600

    login changes

--- models/UserModel.js
+++ models/UserModel.js
@@ -9,7 +9,8 @@ const UserSchema = mongoose.Schema({
     type: String,
     required: true,
     trim: true,
-    // index: { unique: true },
+    index: true,
+    unique: true,
     minlength: 3,
   },
   email: {
@@ -17,7 +18,8 @@ const UserSchema = mongoose.Schema({
     required: true,
     trim: true,
     lowercase: true,
-    // index: { unique: true },
+    index: true,
+    unique: true,
     validate: {
       validator: emailValidator.validate,
       message: (props) => `${props.value} is not a valid email address!`,
@@ -27,25 +29,24 @@ const UserSchema = mongoose.Schema({
     type: String,
     required: true,
     trim: true,
-    // index: { unique: true },
     minlength: 8,
   },
 });
 
-// UserSchema.pre('save', async function preSave(next) {
-//   const user = this;
-//   if (!user.isModified('password')) return next();
-//   try {
-//     const hash = await bcrypt.hash(user.password, SALT_ROUNDS);
-//     user.password = hash;
-//     return next();
-//   } catch (err) {
-//     return next(err);
-//   }
-// });
+UserSchema.pre('save', async function preSave(next) {
+  const user = this;
+  if (!user.isModified('password')) return next();
+  try {
+    const hash = await bcrypt.hash(user.password, SALT_ROUNDS);
+    user.password = hash;
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
 
-// UserSchema.methods.comparePassword = async function comparePassword(candidate) {
-//   return bcrypt.compare(candidate, this.password);
-// };
+UserSchema.methods.comparePassword = async function comparePassword(candidate) {
+  return bcrypt.compare(candidate, this.password);
+};
 
 module.exports = mongoose.model('User', UserSchema);
