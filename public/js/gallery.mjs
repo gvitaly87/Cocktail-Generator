4e841ae661f1caeed42f2a144727ad9bd7b524e2
Vitaly Gins <vitaly.gins@gmail.com>
2021-03-28 14:07:13 -0600

    fixed next page error

--- public/js/gallery.mjs
+++ public/js/gallery.mjs
@@ -1,17 +1,53 @@
+import getRequest from '/js/get-json.mjs';
+
 import deleteDrinks from '/js/delete-items.mjs';
-import getDrinks from '/js/update-gallery.mjs';
 //Default for the gallery to start on page 1 and have 12 items per page
 let pageCount;
+
+const getDrinks = async (page, limit) => {
+  const data = await getRequest(`/api/v0/gallery?page=${page}&limit=${limit}`);
+  const drinkGenerator = data.drinkList;
+  pageCount = data.totalPages;
+  // Set page counter inside the page
+  document.querySelector('.page').innerHTML = `Page ${page} of ${pageCount}`;
+
+  let output = ``;
+  drinkGenerator.forEach(function (drink) {
+    output += `
+    <div class="recipe-card">
+    <figure>
+    <a href="/gallery/${drink.name}">
+    <img class="gallery-image" src="${drink.imgPath}" alt="${drink.name}"></a>
+    </figure>
+    <div class="recipe-data">`;
+
+    for (let i = 0; i < drink.baseAlcohol.length; i++) {
+      output += `
+        <p class="alcohol-type">${drink.baseAlcohol[i].name}</p>`;
+    }
+    // Rounds a random num form 1 to 5 to the nearest decimal
+    const rating = Math.round((Math.random() * 2.5 + 2.5) * 10) / 10;
+    output += `
+      <a href="/gallery/${drink.name}"><h2 class="drink-name">${drink.name}</h2></a>
+      <div class="star-rating">
+      <p class="rating">${rating}<i class="fas fa-star"></i></p>
+      </div>
+    </div>
+    </div>`;
+  });
+  document.querySelector('.drink').innerHTML = output;
+};
+
 let page = 1;
 let entriesPerPage = 12;
-pageCount = getDrinks(page, entriesPerPage);
+getDrinks(page, entriesPerPage);
 
 /***********Page Pagination Buttons*********/
 document.querySelector('.prev').addEventListener('click', () => {
   if (page > 1) {
     page -= 1;
     deleteDrinks('.recipe-card');
-    pageCount = getDrinks(page, entriesPerPage);
+    getDrinks(page, entriesPerPage);
   }
 });
 
@@ -19,7 +55,7 @@ document.querySelector('.next').addEventListener('click', () => {
   if (page < pageCount) {
     page += 1;
     deleteDrinks('.recipe-card');
-    pageCount = getDrinks(page, entriesPerPage);
+    getDrinks(page, entriesPerPage);
   }
 });
 /***********Select Items per page*********/
@@ -28,7 +64,7 @@ entriesSelect.addEventListener('change', () => {
   page = 1;
   entriesPerPage = entriesSelect.value;
   deleteDrinks('.recipe-card');
-  pageCount = getDrinks(page, entriesPerPage);
+  getDrinks(page, entriesPerPage);
 });
 
 /**************Search Button **************/
